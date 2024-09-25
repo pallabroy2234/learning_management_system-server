93ae6c4e57cd60138a454bd71de998fd43619327
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-09-26 00:34:48 +0600

    refactor: if validate related any error than image is delete from the server

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,5 +1,6 @@
 import {validationResult} from "express-validator";
 import {NextFunction, Response, Request} from "express";
+import {deleteImage} from "../middleware/multer";
 
 export const runValidation = (statusCode = 422) => {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -7,10 +8,10 @@ export const runValidation = (statusCode = 422) => {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 // * If there are errors, delete the image from the request
-                // const image = req.file;
-                // if (image) {
-                //     deleteImage(image.path);
-                // }
+                const image = req.file;
+                if (image) {
+                    deleteImage(image.path);
+                }
                 return res.status(statusCode).json({
                     success: false,
                     message: errors.array()[0].msg,
