7d5122d94c5a78a636b1dbf9c922a6249a4b3cf7
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-09-27 03:17:12 +0600

    refactor(course.route.ts): refactor createCourse route add role middleware

--- src/route/course.route.ts
+++ src/route/course.route.ts
@@ -2,20 +2,19 @@ import express from "express";
 import {handleCreateCourse} from "../controller/course.controller";
 import {createCourseValidator} from "../validator/course.validator";
 import {runValidation} from "../validator";
-import {isAuthenticated} from "../middleware/auth";
+import {authorizeRole, isAuthenticated} from "../middleware/auth";
 import {upload} from "../middleware/multer";
 
 
 export const courseRoute = express.Router();
 
 
-
 /**
  * @description          - create a course
  * @route                - /api/v1/course/create
  * @method               - POST
- * @access               - Private
+ * @access               - Private(admin)
  *
-* */
+ * */
 
-courseRoute.post("/create", upload.single("thumbnail"), isAuthenticated, createCourseValidator, runValidation(422), handleCreateCourse)
+courseRoute.post("/create", upload.single("thumbnail"), isAuthenticated, authorizeRole("admin"), createCourseValidator, runValidation(422), handleCreateCourse)
