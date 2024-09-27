ff94bcaf1babacfc83815b76c6e8b0798570b83d
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-09-28 03:45:00 +0600

    feat: create imageUpload utility for image upload to cloudinary

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,6 +1,7 @@
 import logger from "../config/logger";
 import {ErrorHandler} from "./ErrorHandler";
 import {v2 as cloudinary} from "cloudinary"
+import {deleteImage} from "../middleware/multer";
 
 
 export const deleteImageFromCloudinary = async (public_id: string) => {
@@ -15,4 +16,34 @@ export const deleteImageFromCloudinary = async (public_id: string) => {
         return new ErrorHandler("Failed to delete image", 400)
     }
 }
+
+
+interface IImageUpload {
+    path: string
+    folder: string
+    width?: number
+}
+
+export const imageUpload = async ({path, folder, width}: IImageUpload) => {
+    try {
+        let public_id: string;
+        let url: string;
+
+        const result = await cloudinary.uploader.upload(path, {
+            folder: folder,
+            width: width,
+        })
+
+        deleteImage(path)
+        url = result.secure_url;
+        public_id = result.public_id;
+
+        return {url, public_id}
+    } catch (err: any) {
+        logger.error(`imageUpload:${err.message}`)
+        return new ErrorHandler("Failed to upload image", 400)
+    }
+}
+
+
 
