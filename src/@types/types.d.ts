f3ad4812cb5c50740c736ecb11ca2cbba3158d1e
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-09-29 17:29:50 +0600

    feat: type define for add-question request body

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -1,44 +1,49 @@
 import {IUser} from "../model/user.model";
 
 declare global {
-    namespace Express {
-        interface Request {
-            user?: IUser
-            _id?: string
-            role?: string
-        }
-    }
+	namespace Express {
+		interface Request {
+			user?: IUser;
+			_id?: string;
+			role?: string;
+		}
+	}
 }
 
 export interface IRegistrationBody {
-    name: string,
-    email: string,
-    password: string
-    avatar?: string
+	name: string;
+	email: string;
+	password: string;
+	avatar?: string;
 }
 
 export interface IActivationRequest {
-    activation_token: string
-    activation_code: string
+	activation_token: string;
+	activation_code: string;
 }
 
 export interface ILoginRequest {
-    email: string
-    password: string
+	email: string;
+	password: string;
 }
 
 export interface IUpdateUserInfo {
-    name?: string,
-    email?: string,
+	name?: string;
+	email?: string;
 }
 
-
 export interface IUpdatePassword {
-    oldPassword: string,
-    newPassword: string,
+	oldPassword: string;
+	newPassword: string;
 }
 
 export interface ICreatePassword {
-    newPassword: string
-    confirmPassword: string
+	newPassword: string;
+	confirmPassword: string;
+}
+
+export interface IAddQuestionData {
+	question: string;
+	courseId: string;
+	contentId: string;
 }
