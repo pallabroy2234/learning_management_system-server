f39d98643508c72211259f8b9165f56eb670e1a4
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-09-30 03:25:42 +0600

    refactor: refactor sendMail function for utility like use another

--- src/mails/sendMail.ts
+++ src/mails/sendMail.ts
@@ -3,35 +3,49 @@ import {join} from "path";
 import {renderFile} from "ejs";
 import {smtp_host, smtp_mail, smtp_password, smtp_port, smtp_service} from "../secret/secret";
 
+
+
 interface EmailOptions {
-    email: string,
-    subject: string,
-    data: { [key: string]: any }
+	email: string;
+	subject: string;
+	data: { [key: string]: any };
+	template: string;
 }
+
 
 export const sendMail = async (option: EmailOptions) => {
-    const transporter: Transporter = createTransport({
-        host: smtp_host,
-        port: Number(smtp_port || "587"),
-        service: smtp_service,
-        // secure: false,
-        auth: {
-            user: smtp_mail,
-            pass: smtp_password
-        }
-    })
+	const transporter: Transporter = createTransport({
+		host: smtp_host,
+		port: Number(smtp_port || "587"),
+		service: smtp_service,
+		auth: {
+			user: smtp_mail,
+			pass: smtp_password
+		}
+	});
 
-    const {data, subject, email} = option
-    const templatePath = join(__dirname, "../mails/activationMail.ejs")
+	// Destructure the email options
+	const { data, subject, email, template } = option;
 
-    const html = await renderFile(templatePath, {userName: data.name, activationCode: data.activationCode})
+	// Dynamically set the path to the correct EJS template
+	const templatePath = join(__dirname, `./${template}.ejs`);
 
-    const mailOptions = {
-        from: smtp_mail,
-        to: email,
-        subject,
-        html
-    }
-    await transporter.sendMail(mailOptions)
-}
+	// Render the EJS template
+	const html = await renderFile(templatePath, data);
 
+	const mailOptions = {
+		from: smtp_mail,
+		to: email,
+		subject,
+		html
+	};
+
+	// Send the email
+	await transporter.sendMail(mailOptions);
+};
+
+
+// "dev": "concurrently  \"tsc -w\" \"nodemon dist/server.js\"",
+// 	"build": "tsc",
+// 	"vercel-build": "echo Running build command from Vercel",
+// 	"watch": "tsc -w"
