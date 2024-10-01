9793831129eb8708a76c2bd7c899d770f8153d21
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-10-02 02:50:48 +0600

    feat: create notification model

---
+++ src/model/notification.model.ts
@@ -0,0 +1,43 @@
+import {Schema, ObjectId, model, Model, Document} from "mongoose";
+
+export interface INotification extends Document {
+	title: string,
+	message: string,
+	status: "read" | "unread",
+	userId: ObjectId,
+}
+
+
+/**
+ * Notification schema to store notification data
+ * This schema defines the structure of the notification documents in the database.
+ * It includes fields for the title, message, status, and userId, along with timestamps.
+ */
+
+
+const notificationSchema = new Schema<INotification>({
+	title: {
+		type: String,
+		required: [true, "Title is required"]
+	},
+	message: {
+		type: String,
+		required: [true, "Message is required"]
+	},
+	status: {
+		type: String,
+		enum: ["read", "unread"],
+		default: "unread",
+	},
+	userId:{
+		type:Schema.Types.ObjectId,
+		ref:"User",
+		required:[true,"User id is required"]
+	}
+},{timestamps:true});
+
+
+
+export const Notification:Model<INotification> = model("Notification", notificationSchema)
+
+
