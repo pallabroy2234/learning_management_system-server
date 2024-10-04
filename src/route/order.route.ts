53b8acfb92f18b2a14893954989a3397de61bc00
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-10-04 19:07:28 +0600

    feat: add get-all-orders/admin route for get all orders by admin

--- src/route/order.route.ts
+++ src/route/order.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import {isAuthenticated} from "../middleware/auth";
-import {handleCreateOrder} from "../controller/order.controller";
+import {authorizeRole, isAuthenticated} from "../middleware/auth";
+import {handleCreateOrder, handleGetOrdersByAdmin} from "../controller/order.controller";
 import {createOrderValidator} from "../validator/order.validator";
 import {runValidator} from "../validator";
 
@@ -13,3 +13,12 @@ export const orderRoute = express.Router();
  * @access          Private
  * */
 orderRoute.post("/create-order", isAuthenticated, createOrderValidator, runValidator(422), handleCreateOrder);
+
+
+/**
+ * @description     Get all orders by admin
+ * @route           GET /api/order/get-all-orders/admin
+ * @access          Private(only admin)
+ * */
+
+orderRoute.get("/get-all-orders/admin", isAuthenticated, authorizeRole("admin"), handleGetOrdersByAdmin);
