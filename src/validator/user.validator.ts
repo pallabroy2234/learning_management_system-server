d5adb302acb554b130c325d253c363fcf6c4e2b4
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-10-05 02:28:21 +0600

    feat: add updateUserRoleValidator validator for valid request body

--- src/validator/user.validator.ts
+++ src/validator/user.validator.ts
@@ -6,117 +6,129 @@ const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|org|net|edu|gov|mil|
 
 
 export const userRegisterValidator = [
-    body("name")
-        .notEmpty()
-        .withMessage("Please enter your name"),
+	body("name")
+		.notEmpty()
+		.withMessage("Please enter your name"),
 
-    body("email")
-        .notEmpty()
-        .withMessage("Please enter your email")
-        .isEmail()
-        .withMessage("Please enter a valid email")
-        .custom((value) => {
-            if (!emailRegex.test(value)) {
-                throw new Error("Please enter a valid email");
-            }
-            return true;
-        }),
+	body("email")
+		.notEmpty()
+		.withMessage("Please enter your email")
+		.isEmail()
+		.withMessage("Please enter a valid email")
+		.custom((value) => {
+			if (!emailRegex.test(value)) {
+				throw new Error("Please enter a valid email");
+			}
+			return true;
+		}),
 
-    body("password")
-        .notEmpty()
-        .withMessage("Please enter your password")
-        .isLength({min: 6})
-        .withMessage("Password must be at least 6 characters long")
+	body("password")
+		.notEmpty()
+		.withMessage("Please enter your password")
+		.isLength({min: 6})
+		.withMessage("Password must be at least 6 characters long")
 ];
 
 
 export const userActiveValidator = [
-    body("activation_token").notEmpty().withMessage("Activation token is required"),
-    body("activation_code").notEmpty().withMessage("Enter your activation code")
-]
+	body("activation_token").notEmpty().withMessage("Activation token is required"),
+	body("activation_code").notEmpty().withMessage("Enter your activation code")
+];
 
 
 export const userLoginValidator = [
-    body("email")
-        .notEmpty()
-        .withMessage("Please enter your email")
-        .isEmail()
-        .withMessage("Please enter a valid email")
-        .custom((value) => {
-            if (!emailRegex.test(value)) {
-                throw new Error("Please enter a valid email");
-            }
-            return true;
-        }),
-    body("password")
-        .notEmpty()
-        .withMessage("Please enter your password")
-        .isLength({min: 6})
-        .withMessage("Password must be at least 6 characters long")
-]
+	body("email")
+		.notEmpty()
+		.withMessage("Please enter your email")
+		.isEmail()
+		.withMessage("Please enter a valid email")
+		.custom((value) => {
+			if (!emailRegex.test(value)) {
+				throw new Error("Please enter a valid email");
+			}
+			return true;
+		}),
+	body("password")
+		.notEmpty()
+		.withMessage("Please enter your password")
+		.isLength({min: 6})
+		.withMessage("Password must be at least 6 characters long")
+];
 
 
 export const updateUserInfoValidator = [
-    body("name").optional().isString().withMessage("Name must be a string"),
-    body("email")
-        .optional()
-        .isEmail()
-        .withMessage("Please enter a valid email")
-        .custom((value) => {
-            if (!emailRegex.test(value)) {
-                throw new Error("Please enter a valid email");
-            }
-            return true;
-        }),
-]
+	body("name").optional().isString().withMessage("Name must be a string"),
+	body("email")
+		.optional()
+		.isEmail()
+		.withMessage("Please enter a valid email")
+		.custom((value) => {
+			if (!emailRegex.test(value)) {
+				throw new Error("Please enter a valid email");
+			}
+			return true;
+		})
+];
 
 
 export const updatePasswordValidator = [
-    body("oldPassword")
-        .notEmpty()
-        .withMessage("Please enter your old password")
-        .isLength({min: 6})
-        .withMessage("Password must be at least 6 characters long"),
-    body("newPassword")
-        .notEmpty()
-        .withMessage("Please enter your new password")
-        .isLength({min: 6})
-        .withMessage("Password must be at least 6 characters long"),
-    body("newPassword")
-        .custom((newPassword, {req}) => {
-            if (newPassword === req.body.oldPassword) {
-                throw new Error("New password cannot be the same as the old password");
-            }
-            return true;
-        })
+	body("oldPassword")
+		.notEmpty()
+		.withMessage("Please enter your old password")
+		.isLength({min: 6})
+		.withMessage("Password must be at least 6 characters long"),
+	body("newPassword")
+		.notEmpty()
+		.withMessage("Please enter your new password")
+		.isLength({min: 6})
+		.withMessage("Password must be at least 6 characters long"),
+	body("newPassword")
+		.custom((newPassword, {req}) => {
+			if (newPassword === req.body.oldPassword) {
+				throw new Error("New password cannot be the same as the old password");
+			}
+			return true;
+		})
 ];
 
 
 export const createPasswordValidator = [
-    body("newPassword")
-        .notEmpty()
-        .withMessage("Please enter your new password")
-        .isLength({min: 6})
-        .withMessage("Password must be at least 6 characters long"),
-    body("confirmPassword")
-        .notEmpty()
-        .withMessage("Please enter your confirm password")
-        .isLength({min: 6})
-        .withMessage("Password must be at least 6 characters long"),
-    body("confirmPassword").custom((value, {req}) => {
-        if (value !== req.body.newPassword) {
-            throw new Error("Passwords do not match");
-        }
-        return true;
-    })
+	body("newPassword")
+		.notEmpty()
+		.withMessage("Please enter your new password")
+		.isLength({min: 6})
+		.withMessage("Password must be at least 6 characters long"),
+	body("confirmPassword")
+		.notEmpty()
+		.withMessage("Please enter your confirm password")
+		.isLength({min: 6})
+		.withMessage("Password must be at least 6 characters long"),
+	body("confirmPassword").custom((value, {req}) => {
+		if (value !== req.body.newPassword) {
+			throw new Error("Passwords do not match");
+		}
+		return true;
+	})
 ];
 
 
 export const updateAvatarValidator = [
-    body("avatar").custom((value, {req}) => {
-        if (!req.file) {
-            throw new Error("Image is required");
-        }
-        return true;
-    }),
-]
+	body("avatar").custom((value, {req}) => {
+		if (!req.file) {
+			throw new Error("Image is required");
+		}
+		return true;
+	})
+];
+
+/**
+ * @description         - Update user role
+ * @path                - /api/v1/user/update-role/admin
+ * @method              - PUT
+ * @access              - Private(only admin)
+ * */
+
+export const updateUserRoleValidator = [
+	body("id").notEmpty().withMessage("Id is required").isMongoId().withMessage("Invalid id"),
+	body("role").notEmpty().withMessage("Role is required").isIn(["user", "admin"]).withMessage("Role must be either user or admin")
+];
