3f6fb27407232b82d56c3e0dd81e082690862ba3
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-10-10 01:50:44 +0600

    feat: add handleGetCourseAnalytics controller for course analytics

--- src/controller/analytics.controller.ts
+++ src/controller/analytics.controller.ts
@@ -4,6 +4,7 @@ import logger from "../config/logger";
 import {IUser, User} from "../model/user.model";
 import {redisCache} from "../config/redis";
 import {IMonthlyData, lastTwelveMonthsData} from "../utils/analytics";
+import {Course} from "../model/course.model";
 
 
 /**
@@ -15,7 +16,7 @@ import {IMonthlyData, lastTwelveMonthsData} from "../utils/analytics";
 export const handleGetUserAnalytics = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const user = req.user as IUser;
-		const cacheKey = `user:analytics-${user._id}`;
+		const cacheKey = `analytics:user-${user._id}`;
 
 		let userAnalytics: {lastTwelveMonths: IMonthlyData[]};
 
@@ -38,4 +39,34 @@ export const handleGetUserAnalytics = CatchAsyncError(async (req: Request, res: 
 	}
 });
 
+/**
+ * @description        Get course analytics
+ * @route              GET /api/v1/analytics/course-analytics
+ * @method			    GET
+ * @access             Private (only admin)
+ * */
+export const handleGetCourseAnalytics = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const user = req.user as IUser;
+		const cacheKey = `analytics:course-${user._id}`;
 
+		let courseAnalytics: {lastTwelveMonths: IMonthlyData[]};
+
+		if (await redisCache.exists(cacheKey)) {
+			const data = await redisCache.get(cacheKey);
+			courseAnalytics = JSON.parse(data!);
+		} else {
+			courseAnalytics = await lastTwelveMonthsData({model: Course});
+			await redisCache.set(cacheKey, JSON.stringify(courseAnalytics));
+		}
+
+		return res.status(200).json({
+			success: true,
+			message: "Course analytics fetched successfully",
+			payload: courseAnalytics
+		});
+	} catch (err: any) {
+		logger.error(`Error in handleGetCourseAnalytics: ${err.message}`);
+		return next(err);
+	}
+});
