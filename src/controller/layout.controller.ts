4da4fca38d5057825c299100cb95616af5c6519f
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-10-25 21:58:17 +0600

    add handleUpdateFaq controller for update faq

--- src/controller/layout.controller.ts
+++ src/controller/layout.controller.ts
@@ -76,3 +76,62 @@ export const handleCreateLayout = CatchAsyncError(async (req: Request, res: Resp
 		return next(err);
 	}
 });
+
+
+/**
+ * @description       update faq
+ * @route 			  PUT /api/v1/layout/update-faq/:id
+ * @access            Private(Only admin)
+ * */
+export const handleUpdateFaq = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const {type, faq} = req.body as ILayout;
+		const faqId = req.params.id;
+
+		const faqExists = await Layout.findOne({type, _id: faqId});
+		if (!faqExists) return next(new ErrorHandler("FAQ document not found", 404));
+
+		// 	Check existing FAQ or new FAQ
+		const updates = faq.filter((item) => item._id);
+		const newFaq = faq.filter((item) => !item._id);
+
+
+		// updated existing FAQ
+		if (updates.length > 0) {
+			await Promise.all(updates.map(async (item) => {
+				await Layout.updateOne({_id: faqId, "faq._id": item._id}, {
+					$set: {
+						"faq.$.question": item.question,
+						"faq.$.answer": item.answer
+					}
+				}, {new: true, runValidators: true});
+			}));
+		}
+
+
+		//  add new FAQ
+		if (newFaq?.length > 0) {
+			await Layout.updateOne({_id: faqId}, {
+				$push: {
+					faq: {
+						$each: newFaq
+					}
+				}
+			}, {new: true, runValidators: true});
+		}
+
+		// if (deleted?.length > 0) {
+		// 	await Layout.updateOne(
+		// 		{ _id: faqId },
+		// 		{ $pull: { faq: { _id: { $in: deleted } } } }
+		// 	);
+
+		return res.status(200).json({
+			success: true,
+			message: "FAQ updated successfully"
+		});
+	} catch (err: any) {
+		logger.error(`Error in handleUpdateFaq: ${err.message}`);
+		return next(err);
+	}
+});
