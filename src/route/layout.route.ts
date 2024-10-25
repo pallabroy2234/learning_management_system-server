8cca606cc004af04d4fc6ba4c544844155a9bf0d
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2024-10-25 21:59:42 +0600

    add update-faq/:id -route for update faq

--- src/route/layout.route.ts
+++ src/route/layout.route.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import {authorizeRole, isAuthenticated} from "../middleware/auth";
-import {handleCreateLayout} from "../controller/layout.contoller";
-import {layOutValidator} from "../validator/layout.validator";
+import {handleCreateLayout, handleUpdateFaq} from "../controller/layout.controller";
+import {layOutValidator, updateFaqValidator} from "../validator/layout.validator";
 import {runValidator} from "../validator";
 import {upload} from "../middleware/multer";
 
@@ -15,3 +15,12 @@ export const layoutRoute = express.Router();
  * */
 
 layoutRoute.post("/create", upload.single("banner"), isAuthenticated, authorizeRole("admin"), layOutValidator, runValidator(422), handleCreateLayout);
+
+
+/**
+ * @description       update faq
+ * @route 			  PUT /api/v1/layout/update-faq/:id
+ * @access            Private(Only admin)
+ * */
+
+layoutRoute.put("/update-faq/:id", isAuthenticated, authorizeRole("admin"), updateFaqValidator, runValidator(400), handleUpdateFaq);
