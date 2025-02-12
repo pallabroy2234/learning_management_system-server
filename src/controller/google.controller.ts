71aff158beaecf9cbc127749685de9756a7ca3c3
Pallab Roy Tushar <roy.pallabtushar2234@gmail.com>
2025-02-13 00:12:25 +0600

    passport another method to user information get via Google

--- src/controller/google.controller.ts
+++ src/controller/google.controller.ts
@@ -1,7 +1,7 @@
-import passport from "passport";
-import {NextFunction, Request, Response} from "express";
-import {createToken} from "../utils/jsonwebtoken";
-import {redisCache} from "../config/redis";
+// import passport from "passport";
+// import {NextFunction, Request, Response} from "express";
+// import {createToken} from "../utils/jsonwebtoken";
+// import {redisCache} from "../config/redis";
 
 
 /**
@@ -9,7 +9,7 @@ import {redisCache} from "../config/redis";
  * @route             - GET /auth/google
  * @access            - Public
  * */
-export const handleGoogleLogin = passport.authenticate('google');
+// export const handleGoogleLogin = passport.authenticate("google");
 
 
 /**
@@ -18,28 +18,30 @@ export const handleGoogleLogin = passport.authenticate('google');
  * @access            - Public
  *
  * */
-export const handleGoogleCallback = (req: Request, res: Response, next: NextFunction) => {
-    passport.authenticate('google', {session: false}, async (err: any, user: any, info: any) => {
-        if (err) {
-            if (err.name === 'TokenError') {
-                return res.status(400).json({
-                    success: false,
-                    message: "Invalid token.Try again !",
-                });
-            }
-            return next(err);
-        }
-
-        // user info store in cache
-        const cacheKey = `user:${user._id}`;
-        await redisCache.set(cacheKey, JSON.stringify(user));
-
-        // generate token
-        createToken(user, res);
-        res.status(200).json({
-            success: true,
-            message: "Logged in successfully",
-        })
-    })(req, res, next);
-}
+// export const handleGoogleCallback = (req: Request, res: Response, next: NextFunction) => {
+// 	passport.authenticate("google", {session: false}, async (err: any, user: any, info: any) => {
+// 		if (err) {
+// 			if (err.name === "TokenError") {
+// 				return res.status(400).json({
+// 					success: false,
+// 					message: "Invalid token.Try again !"
+// 				});
+// 			}
+// 			return next(err);
+// 		}
+//
+// 		// user info store in cache
+// 		const cacheKey = `user:${user._id}`;
+// 		await redisCache.set(cacheKey, JSON.stringify(user));
+//
+// 		// generate token
+// 		const {accessToken} = createToken(user, res);
+// 		res.status(200).json({
+// 			success: true,
+// 			message: "Logged in successfully",
+// 			payload: user,
+// 			accessToken
+// 		});
+// 	})(req, res, next);
+// };
 
